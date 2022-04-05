6cfae84cd49ae18cd3de4a3bbb84eaa6d31c75b7
Aléctor Alexander <alector.alexandre@gmail.com>
2022-04-05 19:07:52 -0300

    criando requisição de API

--- src/redux/reducer/token.js
+++ src/redux/reducer/token.js
@@ -1,13 +1,11 @@
 import { TOKEN } from '../action';
 
-const INITIAL_STATE = {
-  token: '',
-};
+const INITIAL_STATE = {};
 
 const token = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case TOKEN:
-    return { ...state, token: action.token };
+    return action.token;
   default:
     return state;
   }
