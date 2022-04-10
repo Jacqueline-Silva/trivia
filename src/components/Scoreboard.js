0fc8ad3ab38288ea99b27507b7097bb92d410fa0
Queite <queitesc@gmail.com>
2022-04-10 17:34:24 -0300

    insere imagem tela feedback

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import coulBeBetter from '../image/couldBeBetter.png';
+import wellDone from '../image/wellDone.png';
 
 class Scoreboard extends Component {
   render() {
     const { result, score, assertions } = this.props;
     return (
-      <>
-        <h1 data-testid="feedback-text">{result}</h1>
+      <div>
+        {result === 'Well Done!'
+          ? <img src={ wellDone } alt="charada" />
+          : <img src={ coulBeBetter } alt="charada" />}
         <section>
+          <h1 data-testid="feedback-text">{result}</h1>
           <h2>
             Score:
             {' '}
@@ -23,7 +28,7 @@ class Scoreboard extends Component {
             </span>
           </h2>
         </section>
-      </>
+      </div>
 
     );
   }
