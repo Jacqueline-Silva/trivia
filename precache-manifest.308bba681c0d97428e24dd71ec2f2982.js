8e473ecaf0514a7b3bd3c8e2eca2b0611a1e60d8
Jacqueline Silva <jacque.sx@hotmail.com>
2022-08-23 13:13:42 -0300

    Updates

---
+++ precache-manifest.308bba681c0d97428e24dd71ec2f2982.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa6eeaa9db2275c8dcaef450925d27f7",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "79907714f1a204a1de29",
+    "url": "/trivia/static/css/main.0336928c.chunk.css"
+  },
+  {
+    "revision": "c5e2d5f7fe358ad0c8cd",
+    "url": "/trivia/static/js/2.0f00ee0d.chunk.js"
+  },
+  {
+    "revision": "ca6b5a6a129747daa0202cfb1dcfa4c4",
+    "url": "/trivia/static/js/2.0f00ee0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79907714f1a204a1de29",
+    "url": "/trivia/static/js/main.c4045d79.chunk.js"
+  },
+  {
+    "revision": "8cf3ddda99dc054ff6df",
+    "url": "/trivia/static/js/runtime-main.55dca74b.js"
+  },
+  {
+    "revision": "176b3c333a010cf0c2f579550bcbb95d",
+    "url": "/trivia/static/media/charada-back-verde-e-roxo.176b3c33.png"
+  },
+  {
+    "revision": "46f7f17137fcc2a9597ddf7ac617fbaa",
+    "url": "/trivia/static/media/couldBeBetter.46f7f171.png"
+  }
+]);
